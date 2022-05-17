273cedc3167558488e319845e83323598137f21c
OzoneIsLucky <71771359+OzoneIsLucky@users.noreply.github.com>
2022-05-17 00:29:30 -0400

    added one more demo

--- test.py
+++ test.py
@@ -39,18 +39,19 @@ def checkRange(filter: bloomFilterObject, start: int, stop: int):
     for i in range(start, stop):
         print("checking val: " + str(i) + " " + str(filter.check(i)))
 
-def testFalsePositiveProb(filter: bloomFilterObject, start: int):
+def testFalsePositiveRate(filter: bloomFilterObject, start: int, fp_prob: float):
     # usage: start should be larger than the largest item stored in the filter
     fp_count = 0
-    for i in range(start, start + 10000):
+    sample_size = int(1000 / fp_prob)
+    for i in range(start, start + sample_size):
         if filter.check(i):
             fp_count += 1
-    print("fp_rate: " + str(float(fp_count / 10000)))
+    print("fp_rate: " + str(float(fp_count / sample_size)))
 
 def testFilter(item_count, fp_prob):
     print("demonstration 1: standard use case")
-    smaller_item_count = item_count // 10000
-    larger_fp_prob = fp_prob * 20
+    smaller_item_count = 100
+    larger_fp_prob = 0.2
     print("creating filter with item_count = " + str(smaller_item_count) + " fp_prob = " + str(larger_fp_prob))
     myFilter = bloomFilterObject(smaller_item_count, larger_fp_prob)
     print("bitarray size: " + str(myFilter.getSize()))
@@ -66,26 +67,52 @@ def testFilter(item_count, fp_prob):
     print(myFilter.getBitarray())
     checkRange(myFilter, 1, smaller_item_count + 25)
 
-    print("\n")
+    print("   ---")
 
     print("demonstration 2: exceeding item count")
     print("creating filter with item_count = " + str(item_count) + " fp_prob = " + str(fp_prob))
-    myFilter3 = bloomFilterObject(item_count, fp_prob)
-    print("bitarray size: " + str(myFilter3.getSize()))
-    print("hash func count: " + str(myFilter3.getHashCount()))
+    demo2filter = bloomFilterObject(item_count, fp_prob)
+    print("bitarray size: " + str(demo2filter.getSize()))
+    print("hash func count: " + str(demo2filter.getHashCount()))
     print("fp_rate with bloom filter at 0% capacity")
-    testFalsePositiveProb(myFilter3, item_count + 1)
+    testFalsePositiveRate(demo2filter, item_count + 1, fp_prob)
     for i in range(1, item_count // 2):
-        myFilter3.add(i)
+        demo2filter.add(i)
     print("fp_rate with bloom filter at 50% capacity")
-    testFalsePositiveProb(myFilter3, item_count + 1)
+    testFalsePositiveRate(demo2filter, item_count + 1, fp_prob)
     for i in range(item_count // 2, item_count):
-        myFilter3.add(i)
+        demo2filter.add(i)
     print("fp_rate with bloom filter at 100% capacity")
-    testFalsePositiveProb(myFilter3, item_count + 1)
+    testFalsePositiveRate(demo2filter, item_count + 1, fp_prob)
     for i in range(item_count, item_count * 2):
-        myFilter3.add(i)
+        demo2filter.add(i)
     print("fp_rate with bloom filter at 200% capacity")
-    testFalsePositiveProb(myFilter3, item_count * 2 + 1)
+    testFalsePositiveRate(demo2filter, item_count * 2 + 1, fp_prob)
+
+    print("   ---")
+
+    print("demonstration 3: very small and very large filters")
+    very_small_item_count = 1
+    very_large_item_count = 999999999
+    print("creating filter with item_count = " + str(very_small_item_count) + " fp_prob = " + str(fp_prob))
+    smallFilter = bloomFilterObject(very_small_item_count, fp_prob)
+    print("bitarray size: " + str(smallFilter.getSize()))
+    print("hash func count: " + str(smallFilter.getHashCount()))
+    smallFilter.add(10, True)
+    print("current bitarray:")
+    print(smallFilter.getBitarray())
+    checkRange(smallFilter, 8, 13)
+    print("fp_rate with bloom filter at 100% capacity")
+    testFalsePositiveRate(smallFilter, 20, fp_prob)
+    print("")
+    print("creating filter with item_count = " + str(very_large_item_count) + " fp_prob = " + str(fp_prob))
+    largeFilter = bloomFilterObject(very_large_item_count, fp_prob)
+    print("bitarray size: " + str(largeFilter.getSize()))
+    print("hash func count: " + str(largeFilter.getHashCount()))
+    for i in range(1, int(very_large_item_count / 100)):
+        largeFilter.add(i)
+    print("fp_rate with bloom filter at 1% capacity")
+    testFalsePositiveRate(largeFilter, very_large_item_count, fp_prob)
     
-testFilter(1000000, 0.01)
+    
+testFilter(100000, 0.001)
